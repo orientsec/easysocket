4067e7e399a6de45320839378315af7a5a4dbaa0
梅琥 <meihu@orientsec.com.cn>
2020-09-17 10:16:59 +0800

    修复连接线程池

--- lib/src/main/java/com/orientsec/easysocket/utils/Executors.java
+++ lib/src/main/java/com/orientsec/easysocket/utils/Executors.java
@@ -8,7 +8,6 @@ import androidx.annotation.NonNull;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
