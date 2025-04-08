5071eea55c95395ebc6cb9c5507c68c40ccf12d2
meihu <meihu@orientsec.com.cn>
2025-04-08 11:44:07 +0800

    自动切站点不再检网络状态

--- lib/src/main/java/com/orientsec/easysocket/client/Connector.java
+++ lib/src/main/java/com/orientsec/easysocket/client/Connector.java
@@ -28,19 +28,19 @@ class Connector {
      * Session连接失败或者断开后执行延时的重连。
      */
     void restart(Session session) {
-        if (EasySocket.getInstance().isNetworkAvailable()) {
-            //当前Session连接的服务器不可用的情况下，需要切换到下一个站点。
-            if (!session.isServerAvailable()) {
-                socketClient.switchServer();
-            }
-            if (options.getLivePolicy().autoConnect(socketClient.isActive())) {
-                eventManager.remove(RESTART);
-
-                long delay = options.getConnectInterval();
-                eventManager.publish(RESTART, delay);
-                logger.i("Restart after " + delay + " mill seconds...");
-            }
+        //if (EasySocket.getInstance().isNetworkAvailable()) {
+        //当前Session连接的服务器不可用的情况下，需要切换到下一个站点。
+        if (!session.isServerAvailable()) {
+            socketClient.switchServer();
         }
+        if (options.getLivePolicy().autoConnect(socketClient.isActive())) {
+            eventManager.remove(RESTART);
+
+            long delay = options.getConnectInterval();
+            eventManager.publish(RESTART, delay);
+            logger.i("Restart after " + delay + " mill seconds...");
+        }
+        //}
     }
 
 
