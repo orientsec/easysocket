9784dd2a7549b3a300cfd617bdcd74bad4fcdf86
meihu <meihu@orientsec.com.cn>
2026-09-04 14:04:57 +0800

    项目结构优化

---
+++ app-shared/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    applyDefaultHierarchyTemplate()
+
+    jvmToolchain(17)
+
+    android {
+        namespace = "com.orientsec.easysocket.demo.shared"
+        compileSdk = 37
+        minSdk = 24
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    targets.withType<KotlinNativeTarget>().forEach {
+        it.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.resources)
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(project(":lib"))
+            implementation(project(":lib-platform"))
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.io.core)
+        }
+        androidMain.dependencies {
+            implementation(libs.androidx.appcompat)
+            implementation(libs.androidx.constraint.layout)
+            implementation(libs.androidx.lifecycle.process)
+            implementation(libs.androidx.activity.compose)
+        }
+    }
+}
